fc5a722e069983562560e0759499e4eb5b335e50
Mykhailo Benych <misha.benych@gmail.com>
2022-12-30 20:49:43 +0200

    added encryption to the wallet creation

--- ether-simple-storage-fcc/deploy.js
+++ ether-simple-storage-fcc/deploy.js
@@ -7,8 +7,13 @@ async function main() {
     process.env.RPC_URL
   );
 
-  // it's an open source wallet private key, not best practice, but still
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const encryptedJson = fs.readFileSync("./encryptedKey.json");
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  wallet = await wallet.connect(provider);
+
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
 
   const binary = fs.readFileSync(
